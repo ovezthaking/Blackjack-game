030eb35f3fb25d4b69154579faeeb619d61983aa
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-12-16 23:54:26 +0100

    Zaktualizuj logikę wyświetlania kart w funkcji renderGame, aby używała tablicy cards

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let firstCard = 10
 let secondCard = 4
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
@@ -16,7 +17,7 @@ const startGame = () => {
 
 const renderGame = () =>{
     sumEl.textContent = `Sum: ${sum}`
-    cardsEl.textContent = `Cards: ${firstCard} ${secondCard}`
+    cardsEl.textContent = `Cards: ${cards[0]} ${cards[1]}`
     if (sum <= 20) {
         message = "Do you want to draw a new card? 🙂"
     } else if (sum === 21) {
