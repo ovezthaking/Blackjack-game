163001f220566c6332c96625545be96cca484b7f
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-12-17 04:17:52 +0100

    Dodaj informacje o graczu i jego żetonach do interfejsu gry

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const player = {
+    name: 'OveZo',
+    chips: 200
+}
+
 let cards = []
 let sum = 0
 let hasBlackJack = false
@@ -6,6 +11,9 @@ let message = ""
 let messageEl = document.getElementById('message-el')
 let sumEl = document.querySelector('#sum-el')
 let cardsEl = document.querySelector('#cards-el')
+let playerEl = document.getElementById('player-el')
+
+playerEl.textContent = `${player.name}: $${player.chips}`
 
 
 function getRandomCard() {
